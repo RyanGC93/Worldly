4fb57b67265cf73332fd590d90c34923f748ced5
Ryan Conk <ryangconk@gmail.com>
2021-02-24 20:01:20 -0700

    finished the redux store

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,23 @@
+
+import configureStore from './store'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+
+const store = configureStore()
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>  
   </React.StrictMode>,
   document.getElementById('root')
 );
