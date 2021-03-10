7521b870cc9bb7d869ff9465acd5abf4ecef1034
Ryan Conk <ryangconk@gmail.com>
2021-03-10 13:20:02 -0700

    update the event carousel

--- react-app/src/components/EventCard/EventCarousel/index.js
+++ react-app/src/components/EventCard/EventCarousel/index.js
@@ -8,31 +8,31 @@ const Event = () => {
   const settings =  {
     autoplay: true,
     autoplaySpeed: 3000,
-    dots: true
+    // dots: true
 
   };
   return (
-    <div className='carousel-container'>
-      <div className='carousel-title'>Featured</div>
-      <Slider { ...settings }>
+    <>
+
+      <Slider { ...settings } className="slider-container">
         <div className='slide-container'>
-          <img className='featured-image' alt="West African - style Jollof rice " class="u-photo"  src="https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/4346/letterbox_jollof_TF.jpg"/>
+          <img className='event-image' alt="West African - style Jollof rice "   src="https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/4346/letterbox_jollof_TF.jpg"/>
         </div >
         <div className='slide-container'>
-          <img className='featured-image' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR1hxcP6j2sXLA0_RJZsm67USiTwNfAzPcA&usqp=CAU' />
+          <img className='event-image' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR1hxcP6j2sXLA0_RJZsm67USiTwNfAzPcA&usqp=CAU' />
         </div >
         <div className='slide-container'>
-          <img className='featured-image' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR1hxcP6j2sXLA0_RJZsm67USiTwNfAzPcA&usqp=CAU' />
+          <img className='event-image' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR1hxcP6j2sXLA0_RJZsm67USiTwNfAzPcA&usqp=CAU' />
         </div >
         <div className='slide-container'>
-          <img className='featured-image' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR1hxcP6j2sXLA0_RJZsm67USiTwNfAzPcA&usqp=CAU' />
+          <img className='event-image' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR1hxcP6j2sXLA0_RJZsm67USiTwNfAzPcA&usqp=CAU' />
         </div >
         <div className='slide-container'>
-          <img className='featured-image' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR1hxcP6j2sXLA0_RJZsm67USiTwNfAzPcA&usqp=CAU' />
+          <img className='event-image' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR1hxcP6j2sXLA0_RJZsm67USiTwNfAzPcA&usqp=CAU' />
         </div >
       
       </Slider>
-    </div >
+    </>
   );
 }
 
