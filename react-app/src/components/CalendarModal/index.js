c767b8996d5640544497efbd67c10604e0047be8
Ryan Conk <ryangconk@gmail.com>
2021-03-10 17:47:57 -0700

    update the calendar modal

--- react-app/src/components/CalendarModal/index.js
+++ react-app/src/components/CalendarModal/index.js
@@ -1,21 +1,49 @@
-import React, { useState } from 'react'
-import './styles.css'
-import { Modal } from '../../context/Modal'
-import Calendar from 'react-calendar';
-import {BsCalendar} from 'react-icons/bs'
-function CalendarModal({ edit }) {
-  const [showModal, setShowModal] = useState(false);
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./styles.css";
+function CalendarModal() {
+  const [value, onChange] = useState(new Date());
+  const [toggle, setToggle] = useState(false)
+  
+  const calendarHandler = (value,event) => {
+    alert(value)
+    setToggle(true)
 
+  }
+  
   return (
-    <>
-      <BsCalendar onClick={() => setShowModal(true)} />
-      {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <Calendar setShowModal={setShowModal} edit={edit}/>
-        </Modal>
-      )}
+    <>{!toggle &&
+      <div className="calendar-container">
+        <header className="header">
+          <div> Availabilities</div>
+        </header>
+        <div className="Sample__container">
+          <main className="Sample__container__content">
+            <Calendar
+              className="calendar"
+              onChange={onChange}
+              value={value}
+              onClickDay={calendarHandler}
+            />
+          </main>
+        </div>
+      </div>
+    }
+    {toggle &&
+      <div className="timeslot-container">
+      <header className="header">
+        <div> Time Slots </div>
+      </header>
+      <div className="Sample__container">
+        <main className="Sample__container__content">
+          
+        </main>
+      </div>
+    </div>
+    }
     </>
-  )
+  );
 }
 
 export default CalendarModal;
