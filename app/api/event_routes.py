320862011f126bcb97536ab3c7fbb5dfe2ec4c4a
Ryan Conk <ryangconk@gmail.com>
2021-03-18 00:08:07 -0600

    added the carousel to othe event manager component

--- app/api/event_routes.py
+++ app/api/event_routes.py
@@ -57,31 +57,29 @@ def user_events(user):
           sad
           asdsa
           descriptionsadad''')
-    print(event_values)                                                                                                                          
+    print(event_values) 
+    event_ids = [event[0]for event in event_values]
+                                                                                                   
     user_events_info = {"user_events_info": [dict(zip(event_keys,event)) for event in event_values]}
     
-    # print(user_events_info)
-    return user_events_info
+    photo_gallery_keys = ['photo_id','event_id','photo_description','photo_url']
+    photo_gallery_values = db.session.query(PhotoGallery.id,PhotoGallery.event_id,PhotoGallery.description,PhotoGallery.url).filter(PhotoGallery.event_id.in_(event_ids)).all()
+    photo_gallery = {"photo_gallery": [dict(zip(photo_gallery_keys,photo)) for photo in photo_gallery_values]}    
+    # print(photos_gallery) 
     
+    event_calendar_keys = ['event_calendar_id','event_id', 'date', 'time']
+    event_calendar_values = db.session.query(EventCalendar.id,EventCalendar.event_id,EventCalendar.date,EventCalendar.time).filter(EventCalendar.event_id.in_(event_ids)).all()
+    events_calendar = {"event_calendar": [dict(zip(event_calendar_keys,event_time)) for event_time in event_calendar_values]}    
+    # print(events_calendar) 
     
-    # photo_gallery_keys = ['photo_id','event_id','photo_description','photo_url']
-    # photo_gallery_values = db.session.query(PhotoGallery.id,PhotoGallery.event_id,PhotoGallery.description,PhotoGallery.url).filter(PhotoGallery.event_id.in_(event_ids)).all()
-    # photo_gallery = {"photo_gallery": [dict(zip(photo_gallery_keys,photo)) for photo in photo_gallery_values]}    
-    # # print(photos_gallery) 
-    
-    # event_calendar_keys = ['event_calendar_id','event_id', 'date', 'time']
-    # event_calendar_values = db.session.query(EventCalendar.id,EventCalendar.event_id,EventCalendar.date,EventCalendar.time).filter(EventCalendar.event_id.in_(event_ids)).all()
-    # events_calendar = {"event_calendar": [dict(zip(event_calendar_keys,event_time)) for event_time in event_calendar_values]}    
-    # # print(events_calendar) 
-    
-    # review_keys = ['review_id', 'event_id', 'user_name', 'rating', 'comment', 'created_at']
-    # review_values = db.session.query(Review.id,Review.event_id,User.user_name,Review.rating,Review.comment, Review.date_created).filter(Review.event_id.in_(event_ids), Review.user_id == User.id).all()
-    # reviews = {"reviews": [dict(zip(review_keys,review)) for review in review_values]}
-    # # print(reviews) 
+    review_keys = ['review_id', 'event_id', 'user_name', 'rating', 'comment', 'created_at']
+    review_values = db.session.query(Review.id,Review.event_id,User.user_name,Review.rating,Review.comment, Review.date_created).filter(Review.event_id.in_(event_ids), Review.user_id == User.id).all()
+    reviews = {"reviews": [dict(zip(review_keys,review)) for review in review_values]}
+    # print(reviews) 
 
-    # events = {'events':[events_info,photo_gallery,events_calendar,reviews]} 
-    # # print(events)
-    # return json.dumps(events,  sort_keys=True, default=str)
+    events = {'events':[user_events_info,photo_gallery,events_calendar,reviews]} 
+    # print(events)
+    return json.dumps(events,  sort_keys=True, default=str)
 
 
 
