ee9576bbd247e1ba5bfdbe04fffed8ec58a692b8
Ryan Conk <ryangconk@gmail.com>
2021-03-18 23:48:59 -0600

    addd the landing page banner

---
+++ react-app/src/components/LandingPage/FeatureHeader/index.js
@@ -0,0 +1,33 @@
+import React, { useState,useEffect } from "react";
+import {useSelector} from 'react-redux'
+import './styles.css'
+
+const FeatureHeader = () => {
+
+  return(
+    <>
+      <div className='mainfeed-feature'>
+        <img className='background-img' src='https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/128609611_2778529755736748_3306568680532272501_o.jpg?_nc_cat=102&ccb=1-3&_nc_sid=973b4a&_nc_ohc=n9CJOIK73B0AX_gDJme&_nc_ht=scontent-frt3-1.xx&oh=65543be05183881b985c205858292623&oe=6078C9DB' />  
+        
+        <div className='mainfeed-feature-info' >
+          <div className='feature-header' >
+          </div>
+          <div className='feature-content' >
+            <div className='feature-title'>Be Adventurous</div>
+            <div className='feature-content'>Taste new flavors.             </div>
+            <div className='feature-content'>Experience new cultures.
+            </div>
+            <div className='feature-content'> One bite at a time.</div>
+            <div className='explore-btn-container' >
+              <button className='feature-btn'>Explore</button>
+              </ div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+
+}
+
+export default FeatureHeader
