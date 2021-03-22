1b6d621463cd208fb6921a266dffe56e95557799
Ryan Conk <ryangconk@gmail.com>
2021-03-22 15:16:04 -0600

    add slide container component

---
+++ react-app/src/components/LandingPage/SlideContainer/index.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import "./styles.css";
+
+let countries = require('./countryInfo')
+let africa = countries.africa
+const SlideContainer = () => {
+  
+  
+  const handleClick = () =>{
+      let clicker = document.querySelector('trial')
+  }
+  
+  return (
+    <>
+      <div className="sizing-container">
+        <div className="landing-slide">
+          <div className="slide-title">{africa.name}</div>
+          <div className="content-container trial">
+            <div className="column-left">
+              <div className="slide-pic-container">
+                <img
+                  className="slide-picture"
+                  src={africa.photos[0]}
+                />
+              </div>
+              <div className='scroll-title'>Info</div>
+              <div id="scroll-1" className="country-info-scroll">
+                {africa.info}
+              </div>
+              <div className='explore-btn' onClick={handleClick}>Explore Africa</div>
+            </div>
+            <div className="column-right">
+
+              <div className="grid-pic small-pic-1">
+                                <img
+                  className="slide-picture"
+                  src={africa.photos[1]}
+                />
+              </div>
+              <div className="grid-pic small-pic-2">
+                                <img
+                  className="slide-picture"
+                  src={africa.photos[2]}
+                />
+              </div>
+              <div className="grid-pic small-pic-3">
+              <img
+              className="slide-picture"
+              src={africa.photos[3]}
+            />
+              </div>
+              <div className="grid-pic small-pic-4">
+              <img
+              className="slide-picture"
+              src={africa.photos[4]}
+            />
+              </div>
+              <div className="grid-pic small-pic-5">
+              <img
+                  className="slide-picture"
+                  src={africa.photos[5]}
+                />
+              </div>
+              <div className="grid-pic small-pic-6">
+              <img
+                  className="slide-picture"
+                  src={africa.photos[6]}
+                />
+              </div>
+              {/* <button>See Events</button> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SlideContainer;
