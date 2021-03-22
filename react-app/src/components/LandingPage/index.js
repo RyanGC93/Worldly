4a9e0bfdb5c47480c307aaa1c9b3db10626baed4
Ryan Conk <ryangconk@gmail.com>
2021-03-22 15:30:44 -0600

    created container for the regions card

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import './styles.css'
 import FeatureHeader from './FeatureHeader'
-
+import SlideContainer from "./SlideContainer"
 const LandingPage = () => {
 
     return (
         <>
             <FeatureHeader />
+            <div className='regions-container' />
+            <div className='regions-title'>Take your taste buds on a trip
+            </div>
+                <div className="regions-feed" >
+                    
+                    <SlideContainer />
+                </div>
+
+
 
         </>    
     )
