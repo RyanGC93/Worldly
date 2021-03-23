b9b4f7f3a090a526b52d908a823e9b88ff3e4dc4
Ryan Conk <ryangconk@gmail.com>
2021-03-23 14:34:54 -0600

    add styling to dropdown menu

--- react-app/src/components/NavBar/DropDownMenu/index.js
+++ react-app/src/components/NavBar/DropDownMenu/index.js
@@ -1,5 +1,6 @@
 
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
+import 'styles.css'
 
 export default function DropDownMenu() {
     const dropdownRef = useRef(null);
