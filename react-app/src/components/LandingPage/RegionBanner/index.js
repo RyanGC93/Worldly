d07d6ad1cb85441781e089b742e2c4cb0fe0ed52
Ryan Conk <ryangconk@gmail.com>
2021-03-24 16:39:13 -0600

    add more styling

--- react-app/src/components/LandingPage/RegionBanner/index.js
+++ react-app/src/components/LandingPage/RegionBanner/index.js
@@ -1,13 +1,31 @@
-import React from "react;";
+import React from "react";
 import './styles.css'
 
 const RegionBanner = ({ country }) => {
-    
+	console.log(country)
+	let images = country.photos
+	console.log(photos)
 	return (
 		<>
 			<div className="sizing-container">
 				<div className="landing-slide">
-				
+				<div id="images-container" class="overlay">
+   
+   {images.map((image) => ( 
+   
+	 <div
+	   class="region-feature-img"
+	   id={image}
+	   style={{ backgroundImage:`url(${image})` }}
+	   // style={{backgroundImage: "url("{image}")"}}
+	 ></div>
+
+   ))}
+</div>
+<button onClick={handleClick} >click</ button>
+<button onClick={handleClickTwo} >click</ button>
+
+</div>
 				
 				</div>
 			</div>
