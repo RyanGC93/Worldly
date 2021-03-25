6803a47a43fb06e6d6b3a610348af6811385c444
Ryan Conk <ryangconk@gmail.com>
2021-03-25 00:22:59 -0600

    hooked up transition photos

--- react-app/src/components/LandingPage/RegionBanner/index.js
+++ react-app/src/components/LandingPage/RegionBanner/index.js
@@ -1,36 +1,62 @@
-import React from "react";
-import './styles.css'
+import React, { useState, useEffect } from "react";
+import "./styles.css";
 
 const RegionBanner = ({ country }) => {
-	console.log(country)
-	let images = country.photos
-	console.log(photos)
+	const [childrenList, setChildrenList] = useState("");
+	const [activeChild, setActiveChild] = useState("");
+	let images = country.photos;
+
+	const fadeOut = async () => {
+		let prevAct = document.querySelector(".fade-in");
+		if (prevAct === null) return;
+		prevAct.classList.remove("fade-in");
+		prevAct.classList.add("fade-out");
+	};
+	const fadeIn = async () => {
+		let children = document.getElementById(`${country.short}-imgs`).children;
+		let prev = document.querySelector(".fade-out");
+		let child = children[Math.floor(Math.random() * children.length)];
+		console.log(child, "child");
+
+		if (prev !== null) prev.classList.remove("fade-out");
+
+		child.classList.add("fade-in");
+	};
+
+	const trial = async () => {
+		// alert()
+		let children = document.getElementById(`${country.short}-imgs`).children;
+
+		while (children[0]) {
+			await fadeIn();
+			await new Promise((resolve) => setTimeout(resolve, 10000));
+			await fadeOut();
+			await new Promise((resolve) => setTimeout(resolve, 5000));
+		}
+		//  if(!children[0]) trial()
+	};
+	useEffect(() => {
+		trial()
+	},[])
+
 	return (
 		<>
 			<div className="sizing-container">
 				<div className="landing-slide">
-				<div id="images-container" class="overlay">
-   
-   {images.map((image) => ( 
-   
-	 <div
-	   class="region-feature-img"
-	   id={image}
-	   style={{ backgroundImage:`url(${image})` }}
-	   // style={{backgroundImage: "url("{image}")"}}
-	 ></div>
-
-   ))}
-</div>
-<button onClick={handleClick} >click</ button>
-<button onClick={handleClickTwo} >click</ button>
-
-</div>
-				
+					<div id={`${country.short}-imgs`} className="overlay">
+						{images.map((image) => (
+							<div
+								className="region-feature-img"
+								id={image}
+								style={{ backgroundImage: `url(${image})` }}
+								// style={{backgroundImage: "url("{image}")"}}
+							></div>
+						))}
+					</div>
 				</div>
 			</div>
 		</>
 	);
 };
-    
+
 export default RegionBanner;
