b6600ffd9fbed6f016e90bbb6a95df227aedc47f
Ryan Conk <ryangconk@gmail.com>
2021-04-09 03:20:12 -0600

    add the event of the ambassador

--- react-app/src/components/EventsManagement/index.js
+++ react-app/src/components/EventsManagement/index.js
@@ -1,11 +1,15 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { useSelector } from 'react-redux';
 import Map from "../Map";
+import './styles.css'
+import EventViewer from './EventViewer'
+import Slider from 'infinite-react-carousel'
 
 
 const EventManagement = ({ isChecked }) => {
+
   	const userEvents = useSelector((state) => {
 		if (isChecked) return Object.values(state.userEvents);
 		if (!isChecked) return Object.values(state.ambassadorEvents);
@@ -13,49 +17,69 @@ const EventManagement = ({ isChecked }) => {
 
   useEffect(() => {
     if (!userEvents[0]) return
-    console.log(userEvents)
+
+    
 	}, [userEvents]);
   
+  const contentDivider = (arr) => {
+    let newarr = [];
+    let i,
+      j,
+      temparray,
+      chunk = 2;
+    for (i = 0, j = arr.length; i < j; i += chunk) {
+      temparray = arr.slice(i, i + chunk);
+      newarr.push(temparray);
+    }
+    return newarr;
+  };
+
+  const dividedContent = contentDivider(userEvents)
+  console.log(dividedContent, "divided")
+
+  const events = userEvents.map((event) => event.event_id)
+  const uniqueEvents = Array.from(new Set(events))
 
     return (
       <>
-        <div className='ambassador-container' >
-
-            
-            <Tabs>
+  <Tabs>
     <TabList>
-            <Tab className=''>Map</Tab>
-
-            <Tab className=''>Upcoming Events</Tab>
-
-            
+            <Tab>All Events</Tab>
+            <Tab>Upcoming Events</Tab>
+      <Tab>Map</Tab>
     </TabList>
 
     <TabPanel>
-      <Map isChecked={isChecked} className='map-wrapper' />
+      <h2>Any content 1</h2>
+    </TabPanel>
+          <TabPanel>
+            <div className='ambassador-slider'>
+      <Slider >
+                {dividedContent && dividedContent.map((event) => (
+                  <div className='ambasador-slide'>
+
+                  {
+                    event[0] &&
+                    <EventViewer event={event[0]} />
+                    
+                    }
+                      {
+                    event[1] &&
+                    <EventViewer event={event[1]} />
+                    
+                    }  
+                  </div>
+
+                ))}
+              </Slider>
+
+            </div>
     </TabPanel>
     <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
+      <Map isChecked={isChecked}/>
+    </TabPanel>      
   </Tabs>
 
-
-{/* 
-            <div className='management-container'>
-            <div className='event-creator-container'>
-            
-            </div>
-            <div className='event-viewer'>
-            
-            </div>
-            
-            
-            </div> */}
-            
-
-
-
-            </div>
         </>
     )    
 
