292aa53f25c67ea23a3773feba1dace388d160f1
Ryan Conk <ryangconk@gmail.com>
2021-04-09 12:51:09 -0600

    add some of the routing to the search feature

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -2,12 +2,20 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import db, Review
 
-review_routes = Blueprint('reviews', __name__)
+search_routes = Blueprint('searchs', __name__)
 
 # Gets All the events
-@review_routes.route('/')
+@search_routes.route('/<string:query>')
 @login_required
-def reviews():
+def Events():
+    
+    # Search the Events base off Title
     keys = ['event_id', 'title', 'description', 'region', 'country', 'firstname']
     events = db.session.query(Event.id, Event.title, Event.description, Location.region, Location.country, User.first_name).filter(Location.event_id == Event.id, Ambassador.id == Event.ambassador_id, Ambassador.user_id == User.id).all();
     return {"events": [dict(zip(keys,event)) for event in events]}
+
+    # Search the Events base off location
+    keys = ['event_id', 'title', 'description', 'region', 'country', 'firstname']
+    events = db.session.query(Event.id, Event.title, Event.description, Location.region, Location.country, User.first_name).filter(Location.event_id == Event.id, Ambassador.id == Event.ambassador_id, Ambassador.user_id == User.id).all();
+    return {"events": [dict(zip(keys,event)) for event in events]}
+
