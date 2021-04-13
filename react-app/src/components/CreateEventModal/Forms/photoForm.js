11adce9732c90cbd418d67f5013028bd5f9783d9
Ryan Conk <ryangconk@gmail.com>
2021-04-12 18:59:28 -0600

    update the createEventModels

--- react-app/src/components/CreateEventModal/Forms/photoForm.js
+++ react-app/src/components/CreateEventModal/Forms/photoForm.js
@@ -1,98 +1,139 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
-import { getSignedRequest } from '../../services/upload'
-import { createPost, editPost, deletePost } from '../../Store/posts'
-import './PostForm.css'
+import React, { useState } from "react";
+import styles from './styles.module.css'
+/* Form requires ambassador id title descrition, cost, location(lon,lat) */
+
+const EventForm = ({setFormStep}) => {
+	const [errors, setErrors] = useState([]);
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [cost, setCost] = useState("");
+	const [city, setCity] = useState("");
+	const [country, setCountry] = useState("");
 
 
-function PostForm({ edit, post, setShowModal }) {
-  const history = useHistory()
-  const dispatch = useDispatch()
-  const [src, setSrc] = useState('')
-  const [photo, setPhoto] = useState('')
-  const [description, setDescription] = useState('')
-  const [isPrivate] = useState(false)
-  const user = useSelector(state => state.session.user)
-  let userId
-  if (user) {
-    userId = user.id;
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude,longitude)
   }
-  useEffect(() => {
-    if (post) {
-      setDescription(post.description)
-      setSrc(post.imagePath)
-    }
-  })
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (edit) {
-      await dispatch(editPost(post.id, description, isPrivate));
+
+  const findLocation = () => {
+    if (navigator.geolocation) {
+      let geo = navigator.geolocation.getCurrentPosition(success)
+      console.log(geo)
     } else {
-      const url = await getSignedRequest(photo);
-      await dispatch(createPost({userId, description, url, isPrivate }));
-    }
-    setShowModal(false)
-    history.push(`/profile/${userId}`);
-  };
-  const readUrl = (e) => {
-    if (e.target.files[0]){
-      const src = URL.createObjectURL(e.target.files[0])
-      setSrc(src)
-      setPhoto(e.target.files[0])
+      alert("Geolocation is not supported by this browser.");
     }
   }
-  const removePost = async (e) => {
-    await dispatch(deletePost(post.id));
-  };
 
-  const closeWindow = (e) => {
-    e.preventDefault()
-    setShowModal(false);
+	const geoHandler = async (e) => {
+		e.preventDefault();
+		let replacedCity = city.replaceAll(" ", "+");
+		let replacedCountry = country.replaceAll(" ", "+");
+
+		let url = `https://maps.googleapis.com/maps/api/geocode/json?&address=${replacedCity}%${replacedCountry}&key=${process.env.REACT_APP_GOOGLE_GEO_KEY}`;
+		const response = await fetch(url);
+		if (!response.ok) return;
+		let data = await response.json();
+	};
+
+  const formValidation = () => {
+
+
   }
-  return (
-    <div className="postform__container">
-      <div className="postform__closeForm">
-        <i className="fas fa-window-close" onClick={closeWindow}/>
-      </div>
-      <h2 className="postform__header">{edit ? "Edit Post" : "New Post"}</h2>
-      <form className="postform" onSubmit={handleSubmit}>
-      {src && <img className="postform__image" src={src} />}
-        {!edit && (
-          <div className="fileInput__container">
-            <label className="postform__label fileInput__label">
-              Choose a Photo
-              <input
-                type="file"
-                className="postform__input fileInput"
-                onChange={readUrl}
-              />
-            </label>
+
+	const onSubmit = async (e) => {
+    e.preventDefault();
+    setFormStep(2)
+	};
+	const check = (e) => {};
+
+	return (
+		<form className={styles.formContainer} onSubmit={onSubmit}>
+			<div>Basic Info</div>
+      <div className={styles.errorContainer}>
+				{errors.map((error) => (
+					<div className={styles.error}>{error}</div>
+				))}
+			</div>
+			<div className={styles.group}>
+				<label className={styles.label} htmlFor="title">
+					Title
+				</label>
+				<input
+					required
+					className={styles.input}
+					name="title"
+					type="text"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+			</div>
+			<div className={styles.group}>
+				<label className={styles.label} htmlFor="description">
+					Description
+				</label>
+          <div className={styles.textAreaContainer}>
+            <textarea
+              required
+              className={styles.textInput}
+              name="description"
+              type="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+
           </div>
-        )}
+			</div>
+			<div className={styles.group}>
+				<label className={styles.label} htmlFor="cost">
+					Cost
+				</label>
+				<input
+					required
+					className={styles.input}
+					name="cost"
+          type="number" 
+          min="1" 
+          step="any"
+					value={cost}
+					onChange={(e) => setCost(e.target.value)}
+				/>
+			</div>
+      <div>
+      <div className={styles.label}>Location </div>
+      <div onClick={findLocation}>Find My Location </div>
+      <div className={styles.locationGroup} > 
+      <input
+					className={styles.input}
+          required
+          placeholder="City"
+					name="city"
+					type="city"
+					value={city}
+					onChange={(e) => setCity(e.target.value)}
+				/>
+        	<input
+					className={styles.input}
+          required
+          placeholder="Country"
+					name="country"
+					type="country"
+					value={country}
+					onChange={check}
+					// onChange={(e)=> setCountry(e.target.value)}
+          /> 
 
-        <textarea
-          rows="5"
-          cols="33"
-          type="textarea"
-          value={description}
-          placeholder="Write a caption..."
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <div className="postform__button-container">
-          <button type="submit" className="postform__button">
-            {edit ? "Edit Post" : "Share Post"}
-          </button>
 
-          {edit && (
-            <a onClick={removePost} className="postform__delete">
-              Delete Post
-            </a>
-          )}
-        </div>
-      </form>
-    </div>
-  );
-}
+      </div>
+      </div>
+			<div onClick={geoHandler}>Geo test</div>
+      <div className={styles.stepHandler}>
 
-export default PostForm;
+			    <button type="submit">Next</button>
+      </div>
+		</form>
+	);
+};
+
+export default EventForm;
