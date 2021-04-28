42de65bca4a81b12085300fc77838aa83f30be35
Ryan Conk <ryangconk@gmail.com>
2021-04-28 04:09:46 -0600

    redoing the passport

--- react-app/src/components/Profile/Passport/index.js
+++ react-app/src/components/Profile/Passport/index.js
@@ -1,14 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import HTMLFlipBook from "react-pageflip";
-import "./styles.css"; 
+import "./styles.css";
 import * as userEventActions from "../../../store/userEvents";
 import { useSelector, useDispatch } from "react-redux";
 import Slider from "infinite-react-carousel";
 import { BsFillTrashFill } from "react-icons/bs";
-import {deleteUserEvent} from '../../../store/userEvents'
+import { deleteUserEvent } from "../../../store/userEvents";
 
 import * as ambassadorEventActions from "../../../store/ambassadorEvents";
-
 
 const PageCover = React.forwardRef((props, ref) => {
 	return (
@@ -22,9 +21,9 @@ const PageCover = React.forwardRef((props, ref) => {
 	);
 });
 
-// const Page = React.forwardRef((props, ref) => {
-	const Page = (props) => {
-	const dispatch = useDispatch()
+const Page = React.forwardRef((props, ref) => {
+	// const Page = (props) => {
+	const dispatch = useDispatch();
 	let imagesOne, imagesTwo, contentOne, contentTwo;
 
 	if (props.content) {
@@ -46,13 +45,11 @@ const PageCover = React.forwardRef((props, ref) => {
 	// 	});
 	// }
 	const removeEvent = (id) => {
-		dispatch(deleteUserEvent(id))
-	}
-
-
+		dispatch(deleteUserEvent(id));
+	};
 
 	return (
-		<div className="page"  data-density={props.density | "soft"}>
+		<div className="page" ref={ref} data-density={props.density | "soft"}>
 			<div className="page-header">{props.header} Events </div>
 			<div className="page-content">
 				{contentOne && (
@@ -71,7 +68,9 @@ const PageCover = React.forwardRef((props, ref) => {
 								<div className="page-date">
 									{contentOne.date.slice(5)}, {contentOne.time}
 								</div>
-								<button onClick={()=>removeEvent(contentOne.booking_id)}>DELETE</button>
+								<button onClick={() => removeEvent(contentOne.booking_id)}>
+									DELETE
+								</button>
 								{/* <BsFillTrashFill onClick={} className='trash'/> */}
 							</div>
 						</div>
@@ -109,111 +108,110 @@ const PageCover = React.forwardRef((props, ref) => {
 			</div>
 		</div>
 	);
-// });
-}
-class Passport extends React.Component {
-	constructor(props) {
-		super(props);
+});
 
-		const pages = [<PageCover key={0} pos="top"></PageCover>];
-
-		pages.push(
-			<PageCover key={101} pos="bottom">
-				THE END
-			</PageCover>
-		);
-
-		this.state = {
-			page: 0,
-			totalPage: 0,
-			orientation: "landscape",
-			state: "read",
-			pages: pages,
-		};
-	}
-
-	nextButtonClick = () => {
-		this.flipBook.getPageFlip().flipNext();
+const Passport = (props) => {
+	const dispatch = useDispatch();
+	const removeEvent = (id) => {
+		dispatch(deleteUserEvent(id));
 	};
 
-	prevButtonClick = () => {
-		this.flipBook.getPageFlip().flipPrev();
-	};
-
-	onPage = (e) => {
-		this.setState({
-			page: e.data,
-		});
-	};
-
-	onChangeOrientation = (e) => {
-		this.setState({
-			orientation: e.data,
-		});
-	};
-
-	onChangeState = (e) => {
-		this.setState({
-			state: e.data,
-		});
-	};
-
-	componentDidMount() {
-		this.setState({
-			totalPage: this.flipBook.getPageFlip().getPageCount(),
-		});
-	}
-
-	e;
-	render() {
-		return (
-			<>
-				<div
-					id="passport"
-					className="container-md "
-					style={{ position: "relative" }}
+	return (
+		<>
+			<div
+				id="passport"
+				className="container-md "
+				style={{ position: "relative" }}
+			>
+				<HTMLFlipBook
+					width={550}
+					height={733}
+					size="stretch"
+					minWidth={115}
+					maxWidth={2000}
+					minHeight={100}
+					maxHeight={2533}
+					maxShadowOpacity={0.5}
+					showCover={true}
+					mobileScrollSupport={true}
+					className="flip-book html-book demo-book"
 				>
-					<HTMLFlipBook
-						width={550}
-						height={733}
-						size="stretch"
-						minWidth={115}
-						maxWidth={2000}
-						minHeight={100}
-						maxHeight={2533}
-						maxShadowOpacity={0.5}
-						showCover={true}
-						mobileScrollSupport={true}
-						onFlip={this.onPage}
-						onChangeOrientation={this.onChangeOrientation}
-						onChangeState={this.onChangeState}
-						className="flip-book html-book demo-book"
-						style={{ backgroundImage: "url(images/background.jpg)" }}
-						ref={(el) => (this.flipBook = el)}
-					>
-						<PageCover key={0} pos="top"></PageCover>
+					<PageCover key={0} pos="top"></PageCover>
+					<div>sadasdsadsadsad</div>
 
-						{this.props.pastBookEvents &&
-							this.props.pastBookEvents.map((page, i) => (
-								<Page
-									array={this.props.pastBookEvents}
-									key={`pastBookPage-${i}`}
-									header={"Past"}
-									content={page}
-								/>
-							))}
+					{props.upcomingBookEvents &&
+						props.upcomingBookEvents.map((page, i) => (
+							<div className="page" data-density="soft">
+								<div className="page-header">{page.header} Events </div>
+								<div className="page-content">
+									{page && (
+										<div className="content-section">
+											<button onClick={() => console.log(page)}></button>
 
-						{this.props.upcomingBookEvents &&
-							this.props.upcomingBookEvents.map((page, i) => (
-								<Page
-									key={`pastBookPage-${i}`}
-									header={"Upcoming"}
-									content={page}
-								/>
-							))}
-					</HTMLFlipBook>
-				</div>
-				{/* <div className="container mt-3">
+											<div className="page-carousel">
+												<div className="page-event-info">
+													<div className="page-title">{page[0].title}</div>
+													<div className="page-location">
+														<div className="page-country">
+															{page[0].country}, {page[0].region}
+														</div>
+														<div className="page-region"></div>
+													</div>
+													<div className="page-date">
+														{page[0].date.slice(5)}, {page[0].time}
+													</div>
+													<button >
+														DELETE
+													</button>
+													{/* <BsFillTrashFill onClick={} className='trash'/> */}
+												</div>
+											</div>
+										</div>
+									)}
+									{/* {contentTwo && (
+										<div className="conytent-section">
+											{imagesOne[0] && (
+												<Slider className="page-carousel">
+													<div className="page-event-info">
+														<div className="page-title">{contentTwo.title}</div>
+					
+														<div className="page-location">
+															<div className="page-country">
+																{contentTwo.country}, {contentTwo.region}
+															</div>
+															<div className="page-region"></div>
+														</div>
+														<div className="page-date">{contentTwo.date.slice(5)}</div>
+													</div>
+													{imagesTwo.map((image) => (
+														<div className="page-img-container" key={image.event_id}>
+															<img
+																key={image.photo_id}
+																className="event-image passport-image"
+																alt={image.description}
+																src={image.photo_url}
+															/>
+														</div>
+													))}
+												</Slider>
+											)}
+										</div>
+									)} */}
+								</div>
+							</div>
+						))}
+
+					{props.upcomingBookEvents &&
+						props.upcomingBookEvents.map((page, i) => (
+							<Page
+								key={`pastBookPage-${i}`}
+								header={"Upcoming"}
+								content={page}
+							/>
+						))}
+				</HTMLFlipBook>
+			</div>
+			{/* <div className="container mt-3">
 					<div className="row">
 						<div className="col-md-6">
 							<button
@@ -239,14 +237,13 @@ class Passport extends React.Component {
 						</div>
 					</div>
 				</div> */}
-			</>
-		);
-	}
-}
+		</>
+	);
+};
 
 export default function (props) {
-	const dispatch = useDispatch()
-	const [isLoading, setIsLoading] = useState(true)
+	const dispatch = useDispatch();
+	const [isLoading, setIsLoading] = useState(true);
 	let userEvents = useSelector((state) => {
 		if (props.isChecked) return Object.values(state.userEvents);
 		if (!props.isChecked) return Object.values(state.ambassadorEvents);
@@ -274,14 +271,13 @@ export default function (props) {
 	let upcomingBookEvents = contentDivider(sortedUpcomingEvents);
 	let pastBookEvents = contentDivider(sortedPastEvents);
 	useEffect(() => {
-
-		if (userEvents[0]) setIsLoading(false)
-		console.log(userEvents)
-
-	},[isLoading,userEvents])
+		if(!upcomingBookEvents) return
+		if (userEvents[0]) setIsLoading(false);
+		console.log(userEvents,upcomingBookEvents,pastBookEvents);
+	}, [isLoading, upcomingBookEvents, pastBookEvents, userEvents]);
 	return (
 		<>
-			{userEvents[0] && (
+			{upcomingBookEvents && (
 				<Passport
 					upcomingBookEvents={upcomingBookEvents}
 					pastBookEvents={pastBookEvents}
