a23a39ac2b94a305c3e67bfab345fc03a67904ea
Ryan Conk <ryangconk@gmail.com>
2021-05-06 17:59:16 -0600

    update the calendar component

--- react-app/src/components/NavBar/DropDownMenu/index.js
+++ react-app/src/components/NavBar/DropDownMenu/index.js
@@ -1,11 +1,11 @@
-import React, { useRef, useState } from "react";
-import { useLocation, useHistory } from "react-router-dom";
-import { AiOutlineMenu } from "react-icons/ai";
-import { useSelector } from 'react-redux'
+import React, { useRef, useState } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { useSelector } from 'react-redux';
 
-import { useDetectOutsideClick } from "../../../services/detectOutsideClick";
-import styles from "./styles.module.css";
-import { logout } from "../../../services/auth";
+import { useDetectOutsideClick } from '../../../services/detectOutsideClick';
+import styles from './styles.module.css';
+import { logout } from '../../../services/auth';
 
 const DropDownMenu = ({ setAuthenticated }) => {
 	const onLogout = async (e) => {
@@ -13,14 +13,14 @@ const DropDownMenu = ({ setAuthenticated }) => {
 		setAuthenticated(false);
 	};
 
-  let location = useLocation();
+	let location = useLocation();
 	const history = useHistory();
-	const userId= useSelector((state) => state.session.user.username);
+	const userId = useSelector((state) => state.session.user.username);
 	const dropdownRef = useRef(null);
 	const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
 	const onClick = () => setIsActive(!isActive);
 
-  const profileRedirect = () => {
+	const profileRedirect = () => {
 		let pathname = location.pathname;
 		if (pathname.startsWith(`/profile/${userId}`)) {
 			setIsActive(false);
@@ -30,21 +30,16 @@ const DropDownMenu = ({ setAuthenticated }) => {
 		history.push(`/profile/${userId}`);
 	};
 
-
 	return (
 		<div className={styles.menuContainer}>
-				<div onClick={onClick} className={styles.menuTrigger}>
-					<AiOutlineMenu className={styles.menuIcon} />
-				</div>
-			<nav
-					ref={dropdownRef}
-					className={`${styles.menu} ${isActive ? `${styles.active}` : ""}`}
-				>
+			<div onClick={onClick} className={styles.menuTrigger}>
+				<AiOutlineMenu className={styles.menuIcon} />
+			</div>
+			<nav ref={dropdownRef} className={`${styles.menu} ${isActive ? `${styles.active}` : ''}`}>
 				<ul>
 					<li>
 						<div onClick={profileRedirect}>Profile</div>
 					</li>
-					<li>\ </li>
 					<li>
 						<div onClick={onLogout}>Sign Out</div>
 					</li>
