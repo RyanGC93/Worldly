82af5491bf6a687f7ca8c7bbaa3f54a0da594d98
Ryan Conk <ryangconk@gmail.com>
2021-05-07 22:12:13 -0600

    drop down styled and compln

--- react-app/src/components/CreateEventModal/SelectDropDownMenu/index.js
+++ react-app/src/components/CreateEventModal/SelectDropDownMenu/index.js
@@ -1,13 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState } from 'react';
 // import { useLocation, useHistory } from "react-router-dom";
-import { AiFillSwitcher, AiOutlineMenu } from "react-icons/ai";
+import { AiFillSwitcher, AiOutlineMenu } from 'react-icons/ai';
 // import { useSelector } from 'react-redux'
 
-import { useDetectOutsideClick } from "../../../services/detectOutsideClick";
-import styles from "./styles.module.css";
+import { useDetectOutsideClick } from '../../../services/detectOutsideClick';
+import styles from './styles.module.css';
 
-const SelectDropDownMenu = ({region,setRegion}) => {
-
+const SelectDropDownMenu = ({ region, setRegion }) => {
 	const dropdownRef = useRef(null);
 	const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
 	const onClick = () => {
@@ -15,31 +14,25 @@ const SelectDropDownMenu = ({region,setRegion}) => {
 	};
 
 	const selectionHandler = (selection) => {
-		console.log(selection)
-		debugger
-		setRegion(selection)
-	}
-
+		console.log(selection);
+		setRegion(selection);
+	};
 
 	return (
-		<div className={styles.menuContainer} onClick={onClick} >
-			{region ? region : "Please Select A Region"}
+		<div className={styles.menuContainer} onClick={onClick}>
+			{region ? region : 'Please Select A Region'}
 
-					<div onClick={onClick} className={styles.menuTrigger}>
-				</div>
-			<nav
-					ref={dropdownRef}
-					className={`${styles.menu} ${isActive ? `${styles.active}` : ""}`}
-				>
+			<div onClick={onClick} className={styles.menuTrigger}></div>
+			<nav ref={dropdownRef} className={`${styles.menu} ${isActive ? `${styles.active}` : ''}`}>
 				<ul>
 					<li>
-						<div  onClick={()=>selectionHandler('Africa')}>Africa</div>
+						<div onClick={() => selectionHandler('Africa')}>Africa</div>
 					</li>
 					<li>
-						<div >Asia</div>
+						<div onClick={() => selectionHandler('Asia')}>Asia</div>
 					</li>
 					<li>
-						<div >Asia</div>
+						<div onClick={() => selectionHandler('Europe')}>Europe</div>
 					</li>
 				</ul>
 			</nav>
