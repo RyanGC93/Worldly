72bee140b5eb24002533f89607321b7d6be0c98f
Ryan Conk <ryangconk@gmail.com>
2021-05-08 18:55:33 -0600

    add the frontend aws3 signed request

---
+++ react-app/src/services/upload.js
@@ -0,0 +1,24 @@
+export async function getSignedRequest(photo){
+    let res = await fetch('/sign_s3/?file_name='+photo.name+"&file_type="+photo.type);
+    if (res.ok) {
+      res= await res.json()
+      uploadFile(photo, res.data, res.url);
+    } else {
+      console.error('could not get url')
+    }
+    return res.data.url + res.data.fields.key
+  }
+  
+  export async function uploadFile(file, s3Data, url){
+    const data = new FormData()
+    for(const key in s3Data.fields){
+      data.append(key, s3Data.fields[key]);
+    }
+    data.append('file', file)
+    const res = await fetch(url, {
+      method:'POST',
+      body: data
+    })
+    return res
+  }
+  
