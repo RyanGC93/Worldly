60c7db1a96fe15e69ef9a82b601c2d1f17cb293b
Ryan Conk <ryangconk@gmail.com>
2021-06-03 09:49:19 -0600

    add swl error handling to the reviews api

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,13 +1,18 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import db
+from sqlalchemy import exc
 
 review_routes = Blueprint('reviews', __name__)
 
-# Gets All the events
+# Gets All the Reviews
 @review_routes.route('/')
 @login_required
 def reviews():
-    keys = ['event_id', 'title', 'description', 'region', 'country', 'firstname']
-    events = db.session.query(Event.id, Event.title, Event.description, Location.region, Location.country, User.first_name).filter(Location.event_id == Event.id, Ambassador.id == Event.ambassador_id, Ambassador.user_id == User.id).all()
-    return {"events": [dict(zip(keys,event)) for event in events]}
+    try:
+        keys = ['event_id', 'title', 'description', 'region', 'country', 'firstname']
+        events = db.session.query(Event.id, Event.title, Event.description, Location.region, Location.country, User.first_name).filter(Location.event_id == Event.id, Ambassador.id == Event.ambassador_id, Ambassador.user_id == User.id).all()
+        return {"events": [dict(zip(keys,event)) for event in events]}
+    except exc.SQLAlchemyError as e:
+        print(type(e))
+        return {'errors': ['Cannot Get Reviews Please Try again']}, 500
